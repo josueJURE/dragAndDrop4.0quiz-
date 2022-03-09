4f91919cc18a3bf5a371dba04b095e7c39da4b98
josueJURE <josue.jure@gmail.com>
2022-03-09 13:31:36 +0000

    create an array of objects named questions

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("Hello")
+
+let questions = [
+
+  {
+    question: "How many Grand Slam men's singles titles has he won?",
+    choiceA: 10,
+    choiceB: 15,
+    choiceC: 20,
+    questionImg: "url(images/federer.jpeg)",
+    correctAnswer: 20,
+  },
+
+  {
+    question: "How many Formula One World Championship has he won?",
+    choiceA: 5,
+    choiceB: 7,
+    choiceC: 10,
+    questionImg: "url(images/hamilton.jpeg)",
+    correctAnswer: 7,
+  },
+
+  {
+    question: "How many NBA title has Lebron won?",
+    choiceA: 6,
+    choiceB: 3,
+    choiceC: 4,
+    questionImg: "url(images/LeBron.png)",
+    correctAnswer: 4,
+  },
+
+  {
+    question: "How many Ballon D'or has the Argentinina won?",
+    choiceA: 5,
+    choiceB: 6,
+    choiceC: 7,
+    questionImg: "url(images/LeBron.png)",
+    correctAnswer: 7,
+  },
+
+]
+
+console.log(questions[1].question)
